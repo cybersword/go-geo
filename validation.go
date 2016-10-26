0a14b257ed5f5bc7ab0feb75e47d0239e82d8125
wangyu34 <wangyu34@baidu.com>
2016-10-27 01:10:18 +0800

    增加任务包查询, 暂时先写在一起

--- validation.go
+++ validation.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 
+	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/mattn/go-sqlite3"
 )
+
+// TaskPackageInfo 解析 JSON 用
+type TaskPackageInfo struct {
+	Code int
+	Msg  string
+	Data map[string]string
+}
 
 // GKDataType 果壳数据规格, 表结构等
 type GKDataType uint32
@@ -120,6 +132,86 @@ func init() {
 	}
 }
 
+//
+func download(url string, dir string, name string) error {
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	f, err := os.Create(filepath.Join(dir, name))
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(f, res.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getTaskPackageInfoFromMySQL(taskid string) (tpi [2]TaskPackageInfo, err error) {
+	// mysql -hgzns-ns-map-guoke46.gzns -uroot -proot guoke_dawn
+	db, err := sql.Open("mysql", "root:root@tcp(gzns-ns-map-guoke46.gzns.baidu.com:3306)/guoke_dawn?charset=utf8")
+	if err != nil {
+		return
+	}
+	rows, err := db.Query("SELECT down_pir_id, up_pir_id FROM task WHERE task_id=" + taskid)
+	if err != nil {
+		return
+	}
+	if !rows.Next() {
+		err = errors.New("task_id not exists: " + taskid)
+		return
+	}
+	var downloadID string
+	var uploadID string
+	err = rows.Scan(&downloadID, &uploadID) // 131595, 131892
+	if err != nil {
+		return
+	}
+	fmt.Println(downloadID, uploadID)
+
+	return
+}
+
+func getTaskPackage(taskid string, dir string) (path string, err error) {
+	urlQuery := "http://guoke.map.baidu.com/api/mica/api/fix?action=getdawntask&task_id=" + taskid
+
+	resp, err := http.Get(urlQuery)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	htmlData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	//fmt.Println(string(htmlData))
+	var info TaskPackageInfo
+	err = json.Unmarshal(htmlData, &info)
+	if err != nil {
+		return
+	}
+	code := info.Code
+	if code != 0 {
+		err = errors.New(info.Msg)
+		return
+	}
+	urlTask := info.Data["internal_url"]
+	name := "task_pkg_" + taskid + ".zip"
+	path = filepath.Join(dir, taskid)
+	err = os.Mkdir(path, 0777)
+	if err != nil {
+		return
+	}
+	err = download(urlTask, path, name)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 // 返回目录中所有指定类型的文件, 暂时直接返回 sqlite3 类型
 func getGKDatas(dir string, gkDataType GKDataType) []Sqlite3 {
 	var ext string
@@ -161,12 +253,12 @@ func main() {
 			}
 		}(&exto)
 	}
-	errNUm := 0
+	numErr := 0
 	for range extoList {
 		select {
 		case t := <-chErr:
 			fmt.Println(t, "Error")
-			errNUm++
+			numErr++
 		case t := <-chOK:
 			fmt.Println(t, "OK")
 			continue
@@ -174,5 +266,14 @@ func main() {
 	}
 	close(chErr)
 	close(chOK)
-	fmt.Printf("err num : %v / %v \n", errNUm, len(extoList))
+	fmt.Printf("err num : %v / %v \n", numErr, len(extoList))
+
+	taskID := "93721"
+	_, err := getTaskPackageInfoFromMySQL(taskID)
+
+	//path, err := getTaskPackage(taskID, "/Users/baidu/work")
+	if err != nil {
+		fmt.Println("download faild : ", err)
+	}
+	//fmt.Println("download : ", path)
 }
